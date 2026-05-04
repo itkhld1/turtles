b9dda01f670602e618d9d8cde803e762d3ab299c
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2026-05-04 13:27:18 +0300

    Initial Commit

---
+++ sea turtles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sea turtles
+//
+//  Created by itkhld on 4.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sea turtles/sea_turtlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  sea_turtlesApp.swift
+//  sea turtles
+//
+//  Created by itkhld on 4.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct sea_turtlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
